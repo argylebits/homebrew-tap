6801a1e7bc2721baf9f05331743ab6834fbfbcc4
Jim Schultz <jim@argylebits.com>
2026-03-10 03:38:39 +0000

    Update kolache formula to 0.1.1

--- Formula/kolache.rb
+++ Formula/kolache.rb
@@ -1,16 +1,16 @@
 class Kolache < Formula
   desc "Project scaffolding for Swift developers"
   homepage "https://github.com/argylebits/Kolache"
-  version "0.1.0"
+  version "0.1.1"
   license "MIT"
 
   on_macos do
     if Hardware::CPU.arm?
       url "https://github.com/argylebits/Kolache/releases/download/v#{version}/kolache-macos-arm64.tar.gz"
-      sha256 "f01d5d88641fc7c5745abc5734d19d538312207bd2054c356850f0ae2177d3ee"
+      sha256 "0486d6e0a9e10c2f4297503d29eb06a5663fb136028d6754df6c8e335454cdb2"
     elsif Hardware::CPU.intel?
       url "https://github.com/argylebits/Kolache/releases/download/v#{version}/kolache-macos-x86_64.tar.gz"
-      sha256 "86d1cff7cd14b60664ca933a288e6e1a68920f4d418f6eb1b1833dddb821cc97"
+      sha256 "cee6dded77e7ab15b0ad72dfd5f5c0bd1e9be21097886e1c6cc49464b2f85e50"
     end
   end
 
