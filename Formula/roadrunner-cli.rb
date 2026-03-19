9591fa18c4e50ce0b4b0f3a20fe3aa807cbf7ce1
Jim Schultz <jim@argylebits.com>
2026-03-19 04:07:06 +0000

    Update roadrunner-cli formula to 0.1.2

--- Formula/roadrunner-cli.rb
+++ Formula/roadrunner-cli.rb
@@ -1,14 +1,14 @@
 class RoadrunnerCli < Formula
   desc "Ephemeral Linux CI runners on macOS"
   homepage "https://github.com/argylebits/RoadrunnerCLI"
-  version "0.1.1"
+  version "0.1.2"
   license "Apache-2.0"
 
   depends_on :macos
   depends_on arch: :arm64
 
   url "https://github.com/argylebits/RoadrunnerCLI/releases/download/v#{version}/roadrunner-macos-arm64.tar.gz"
-  sha256 "3d7d8558e825e04759126ba5cef50b4328b039b2be4e7a1d56e3794c57251118"
+  sha256 "a5c04685f2f4de6949f07afb545969600b31a43f0a8eab6c2836c25690dfbe98"
 
   def install
     bin.install "roadrunner"
